58b0010cdf0324153d83fd083ccf8e665cfba6c6
T0hsakaRin <1023770070@qq.com>
2020-09-06 19:39:29 +0800

    fix: react

--- react/ToyReact.js
+++ react/ToyReact.js
@@ -1,21 +1,78 @@
-export let ToyReact = {
-	createElement(type, attributes, ...children) {
-		let element = document.createElement(type);
+class ElementWrapper {
+	constructor(type) {
+		this.root = document.createElement(type);
+	}
+	setAttribute(name, value) {
+		this.root.setAttribute(name, value);
+	}
+	appendChild(component) {
+		console.log("component", component);
 
-		for (const name in attributes) {
-			element.setAttribute(name, attributes[name]);
+		this.root.appendChild(component.root);
+	}
+}
+
+class TextWrapper {
+	constructor(content) {
+		console.log("content", content);
+
+		this.root = document.createTextNode(content);
+	}
+}
+
+export class Component {
+	constructor(content) {
+		this.props = Object.create(null);
+		this.children = [];
+		this._root = null;
+	}
+	setAttribute(name, value) {
+		this.props[name] = value;
+	}
+	appendChild(component) {
+		this.children.push(component);
+	}
+	get root() {
+		if (!this._root) {
+			this._root = this.render().root;
 		}
-		for (const child of children) {
-			if (typeof child === "string") child = document.createTextNode(child);
+		return this._root;
+	}
+}
 
-			element.appendChild(child);
+export function createElement(type, attributes, ...children) {
+	console.log("type", type);
+	console.log("children", children);
+
+	let e;
+	if (typeof type === "string") {
+		e = new ElementWrapper(type);
+	} else {
+		e = new type();
+	}
+	for (let p in attributes) {
+		e.setAttribute(p, attributes[p]);
+	}
+	let insertChildred = (children) => {
+		for (let child in children) {
+			if (typeof child === "string") {
+				console.log("child", child);
+
+				child = new TextWrapper(child);
+			}
+			if (typeof child === "object" && child instanceof Array) {
+				insertChildred(child);
+			} else {
+				e.appendChild(child);
+			}
 		}
-		console.log("element", element);
+	};
+	insertChildred(children);
+	console.log("eeeee", e);
 
-		// debugger;
-		return element;
-	},
-	render(vdom, element) {
-		vdom.mountTo(element);
-	},
-};
+	return e;
+}
+
+export function render(component, parentElement) {
+	parentElement.appendChild(component.root);
+}
--- react/main.js
+++ react/main.js
@@ -1,43 +1,20 @@
-import { ToyReact } from "./ToyReact";
+import { createElement, Component, render } from "./ToyReact";
 
-class MyConponent {}
+class MyConponent extends Component {
+	render() {
+		return (
+			<div>
+				<h1>MyConponent</h1>
 
-let a = (
-	<div name="a" id="ida">
-		<span style="color:red">a</span>
-		<span>b</span>
-		<span>c</span>
-	</div>
+				{/* {this.children} */}
+			</div>
+		);
+	}
+}
+
+render(
+	<MyConponent id="a" class="c">
+		<div>abc</div>
+	</MyConponent>,
+	document.body
 );
-
-//这儿a会被翻译为
-// var a = ToyReact.createElement("div");
-
-document.body.appendChild(a);
-
-console.log("a is", a);
-
-// var a = createElement(MyConponent, {
-// 	name: "a",
-// });
-
-//以下为webpack打包之后的东西
-// function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-// // import { ToyReact } from "./ToyReact";
-// var MyConponent = function MyConponent() {
-//   _classCallCheck(this, MyConponent);
-// };
-
-// var a = ToyReact.createElement("div", {
-//   name: "a",
-//   id: "ida"
-// }, ToyReact.createElement("span", {
-//   style: "color:red"
-// }, "a"), ToyReact.createElement("span", null, "b"), ToyReact.createElement("span", null, "c")); //è¿™å„¿aä¼šè¢«ç¿»è¯‘ä¸º
-// // var a = ToyReact.createElement("div");
-
-// document.body.appendChild(a);
-// console.log("a is", a); // var a = createElement(MyConponent, {
-// // 	name: "a",
-// // });
